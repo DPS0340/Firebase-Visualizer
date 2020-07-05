ec368def7202e8a46bb2cbd8b0b213065397f3d0
jiho lee <jiholee@kpu.ac.kr>
2020-07-05 14:06:12 +0900

    뷰어 기능 완성

--- app/src/main/java/xyz/dps0340/firestore_visualizer/MainActivity.kt
+++ app/src/main/java/xyz/dps0340/firestore_visualizer/MainActivity.kt
@@ -1,39 +1,31 @@
 package xyz.dps0340.firestore_visualizer
 
-import android.content.Intent
-import android.net.Uri
+import android.os.Build
 import android.os.Bundle
-import android.util.Log
 import android.widget.Button
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
-import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.firestore.FirebaseFirestore
 import org.jetbrains.anko.startActivity
 import org.jetbrains.anko.toast
 
 
 class MainActivity : AppCompatActivity() {
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         // 기본 액션바 hide
         supportActionBar?.hide()
-        // Firestore logging 허용
-        FirebaseFirestore.setLoggingEnabled(true)
 
-        val auth = FirebaseAuth.getInstance().currentUser
 
-        if(auth != null) {
-            Log.i("FIREBASE", "Current user is $auth")
-        } else {
-            Log.i("FIREBASE", "Authentication failed")
-        }
+        val isApiKeyExists: Int = resources
+            .getIdentifier("google_api_key", "string", packageName)
 
 
         val viewerButton = findViewById<Button>(R.id.viewer_button)
 
         viewerButton.setOnClickListener {
-            if(auth == null) {
+            if(isApiKeyExists == 0) {
                 toast("인증 오류: app 폴더에 google-services.json을 추가 후 진행하세요. Firebase에서 발급받을 수 있습니다.")
                 return@setOnClickListener
             }
--- app/src/main/java/xyz/dps0340/firestore_visualizer/ViewerActivity.kt
+++ app/src/main/java/xyz/dps0340/firestore_visualizer/ViewerActivity.kt
@@ -1,20 +1,100 @@
 package xyz.dps0340.firestore_visualizer
 
+import android.content.Context
+import android.opengl.Visibility
 import android.os.Bundle
-import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.google.android.material.snackbar.Snackbar
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.LinearLayout
+import android.widget.TableLayout
 import androidx.appcompat.app.AppCompatActivity
+import androidx.cardview.widget.CardView
+import com.google.firebase.database.*
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
+import kotlinx.android.synthetic.main.data_layout.*
+import kotlinx.android.synthetic.main.data_layout.view.*
+import kotlinx.android.synthetic.main.toolbar_main.view.*
+import org.jetbrains.anko.image
+import org.jetbrains.anko.imageResource
+
 
 class ViewerActivity : AppCompatActivity() {
+    private lateinit var database: FirebaseDatabase
+    private lateinit var root: DatabaseReference
+    private lateinit var contentLayout: LinearLayout
+    private lateinit var inflater: LayoutInflater
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_viewer)
-        setSupportActionBar(findViewById(R.id.toolbar))
+        // 기본 액션바 hide
+        supportActionBar?.hide()
 
-        findViewById<FloatingActionButton>(R.id.fab).setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
+        database = Firebase.database
+        root = database.getReference("/")
+        inflater = getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+        contentLayout = findViewById<LinearLayout>(R.id.contentLayout)
+
+        root.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                refreshDataView()
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                // Failed to read value
+                Log.w("FIREBASE", "Failed to read value.", error.toException())
+            }
+        })
+    }
+
+    private fun refreshDataView() {
+        contentLayout.removeAllViews()
+        root.addListenerForSingleValueEvent(object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                refreshCore(contentLayout, dataSnapshot)
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                // Failed to read value
+                Log.w("FIREBASE", "Failed to read value.", error.toException())
+            }
+        })
+    }
+
+    private fun refreshSubData(Parent: View, dataSnapshot: DataSnapshot) {
+        Parent.cardView.contentView.data.text = ""
+        Parent.cardView.contentView.data.visibility = View.GONE
+        val nestedLayout = Parent.cardView.contentView.nestedLayout
+        nestedLayout.visibility = View.VISIBLE
+        refreshCore(nestedLayout, dataSnapshot)
+    }
+
+    private fun refreshCore(Layout: LinearLayout, dataSnapshot: DataSnapshot) {
+        for (child in dataSnapshot.children) {
+            val newItem = inflater.inflate(R.layout.data_layout, null)
+            newItem.layoutParams = TableLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT, 1f)
+            newItem.cardView.titleView.dataName.text = child.ref.toString().substring(child.ref.root.toString().length)
+            newItem.cardView.contentView.data.text = child.value.toString()
+            if(dataSnapshot.child(child.key.toString()).hasChildren()) {
+                Log.i("FIREBASE", "${child.key} is nested object")
+                refreshSubData(newItem, dataSnapshot.child(child.key.toString()))
+            } else {
+                newItem.cardView.contentView.data.text = child.value.toString()
+            }
+            newItem.cardView.titleView.setOnClickListener {
+                if(newItem.cardView.contentView.visibility == View.VISIBLE) {
+                    newItem.cardView.contentView.visibility = View.GONE
+                    newItem.cardView.titleView.dropdown.imageResource = R.drawable.down_arrow
+                } else {
+                    newItem.cardView.contentView.visibility = View.VISIBLE
+                    newItem.cardView.titleView.dropdown.imageResource = R.drawable.up_arrow
+                }
+            }
+            Layout.addView(newItem)
+            Layout.invalidate()
         }
     }
 }
