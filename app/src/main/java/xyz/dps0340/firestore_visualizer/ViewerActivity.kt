72c8b274d7325880acb07f358af2f6acd8a86943
jiho lee <jiholee@kpu.ac.kr>
2020-07-05 17:09:50 +0900

    ViewerActivity 오버플로우시 스크롤 적용, 루트 레이아웃 초기화

--- app/src/main/java/xyz/dps0340/firestore_visualizer/ViewerActivity.kt
+++ app/src/main/java/xyz/dps0340/firestore_visualizer/ViewerActivity.kt
@@ -1,7 +1,6 @@
 package xyz.dps0340.firestore_visualizer
 
 import android.content.Context
-import android.opengl.Visibility
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
@@ -10,20 +9,17 @@ import android.view.ViewGroup
 import android.widget.LinearLayout
 import android.widget.TableLayout
 import androidx.appcompat.app.AppCompatActivity
-import androidx.cardview.widget.CardView
 import com.google.firebase.database.*
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
-import kotlinx.android.synthetic.main.data_layout.*
 import kotlinx.android.synthetic.main.data_layout.view.*
-import kotlinx.android.synthetic.main.toolbar_main.view.*
-import org.jetbrains.anko.image
 import org.jetbrains.anko.imageResource
 
 
 class ViewerActivity : AppCompatActivity() {
     private lateinit var database: FirebaseDatabase
     private lateinit var root: DatabaseReference
+    private lateinit var rootLayout: LinearLayout
     private lateinit var contentLayout: LinearLayout
     private lateinit var inflater: LayoutInflater
 
@@ -38,6 +34,8 @@ class ViewerActivity : AppCompatActivity() {
         inflater = getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
         contentLayout = findViewById<LinearLayout>(R.id.contentLayout)
 
+        initRootLayout()
+
         root.addValueEventListener(object : ValueEventListener {
             override fun onDataChange(dataSnapshot: DataSnapshot) {
                 refreshDataView()
@@ -50,11 +48,32 @@ class ViewerActivity : AppCompatActivity() {
         })
     }
 
+    private fun initRootLayout() {
+        val rootItem = inflater.inflate(R.layout.data_layout, null)
+
+        rootItem.cardView.titleView.dataName.text = "/"
+        rootItem.cardView.contentView.data.text = ""
+        rootItem.cardView.contentView.data.visibility = View.GONE
+        rootLayout = rootItem.cardView.contentView.nestedLayout
+        rootLayout.visibility = View.VISIBLE
+        contentLayout.addView(rootItem)
+        contentLayout.invalidate()
+        rootItem.cardView.titleView.setOnClickListener {
+            if(rootItem.cardView.contentView.visibility == View.VISIBLE) {
+                rootItem.cardView.contentView.visibility = View.GONE
+                rootItem.cardView.titleView.dropdown.imageResource = R.drawable.down_arrow
+            } else {
+                rootItem.cardView.contentView.visibility = View.VISIBLE
+                rootItem.cardView.titleView.dropdown.imageResource = R.drawable.up_arrow
+            }
+        }
+    }
+
     private fun refreshDataView() {
-        contentLayout.removeAllViews()
+        rootLayout.removeAllViews()
         root.addListenerForSingleValueEvent(object : ValueEventListener {
             override fun onDataChange(dataSnapshot: DataSnapshot) {
-                refreshCore(contentLayout, dataSnapshot)
+                refreshCore(rootLayout, dataSnapshot)
             }
 
             override fun onCancelled(error: DatabaseError) {
@@ -64,15 +83,15 @@ class ViewerActivity : AppCompatActivity() {
         })
     }
 
-    private fun refreshSubData(Parent: View, dataSnapshot: DataSnapshot) {
-        Parent.cardView.contentView.data.text = ""
-        Parent.cardView.contentView.data.visibility = View.GONE
-        val nestedLayout = Parent.cardView.contentView.nestedLayout
+    private fun refreshSubData(parent: View, dataSnapshot: DataSnapshot) {
+        parent.cardView.contentView.data.text = ""
+        parent.cardView.contentView.data.visibility = View.GONE
+        val nestedLayout = parent.cardView.contentView.nestedLayout
         nestedLayout.visibility = View.VISIBLE
         refreshCore(nestedLayout, dataSnapshot)
     }
 
-    private fun refreshCore(Layout: LinearLayout, dataSnapshot: DataSnapshot) {
+    private fun refreshCore(layout: LinearLayout, dataSnapshot: DataSnapshot) {
         for (child in dataSnapshot.children) {
             val newItem = inflater.inflate(R.layout.data_layout, null)
             newItem.layoutParams = TableLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT, 1f)
@@ -93,8 +112,8 @@ class ViewerActivity : AppCompatActivity() {
                     newItem.cardView.titleView.dropdown.imageResource = R.drawable.up_arrow
                 }
             }
-            Layout.addView(newItem)
-            Layout.invalidate()
+            layout.addView(newItem)
+            layout.invalidate()
         }
     }
 }
