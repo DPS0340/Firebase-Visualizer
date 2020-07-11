70d2e1ec2a13d4a83f58d8d30a06e14ba885a788
jiho lee <jiholee@kpu.ac.kr>
2020-07-11 18:10:15 +0900

    뷰어에서 Firebase DB 키값이 인코딩된 문자열로 나오는 문제를 수정

--- app/src/main/java/xyz/dps0340/firebase_visualizer/ViewerActivity.kt
+++ app/src/main/java/xyz/dps0340/firebase_visualizer/ViewerActivity.kt
@@ -14,6 +14,7 @@ import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
 import kotlinx.android.synthetic.main.data_layout.view.*
 import org.jetbrains.anko.imageResource
+import java.net.URLDecoder
 
 
 class ViewerActivity : AppCompatActivity() {
@@ -95,7 +96,7 @@ class ViewerActivity : AppCompatActivity() {
         for (child in dataSnapshot.children) {
             val newItem = inflater.inflate(R.layout.data_layout, null)
             newItem.layoutParams = TableLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT, 1f)
-            newItem.cardView.titleView.dataName.text = child.ref.toString().substring(child.ref.root.toString().length)
+            newItem.cardView.titleView.dataName.text = URLDecoder.decode(child.ref.toString().substring(child.ref.root.toString().length), "UTF-8")
             newItem.cardView.contentView.data.text = child.value.toString()
             if(dataSnapshot.child(child.key.toString()).hasChildren()) {
                 Log.i("FIREBASE", "${child.key} is nested object")
